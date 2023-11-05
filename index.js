cb8db9b0b4d8b197394b1b0c9206256045e74686
jbruce-doaneu <jared.bruce@doane.edu>
2023-11-04 23:35:12 -0400

    adding the new code for the job

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port= 4000;
+const db = require('.routes/query.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    {
+        extended:true,
+    }
+));
+
+app.get('/', (request, response) => {
+    response.json({info: 'Node.js, Express, and Postgres API'});
+}); 
+
+app.get('/users', db.getUsers);
+
+app.listen(port , () => {
+    console.log(`App running on port ${port}`);
+});
+
+
+
---
+++ routes/query.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres',
+    host: '172.25.0.3',
+    database: 'api',
+    password: 'test1234',
+    port: 5432
+})
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results)=>{
+        if(error){
+            throw error;
+        };
+        response.status(200).json(results.rows);
+    });
+};
+
+module.exports = {getUsers};
