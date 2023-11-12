701d177ec955f448feeafec04fdaf3a25ea18767
jbruce-doaneu <jared.bruce@doane.edu>
2023-11-12 00:07:06 -0500

    write

--- routes/write.js
+++ routes/write.js
@@ -4,17 +4,22 @@ const app = express();
 const pool = require('./db.js');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+    {
+        extended:true,
+    }
+));
 
 const addMovement = (request, response) => {
     console.log(request.body);
     
-    const {Name,Function} = request.body;
+    const { name, program } = request.query;
 
-    pool.query('INSERT INTO "Movement" ("Name","Function") VALUES ($1, $2)', [Name, Function], (error, results) => {
+    pool.query('INSERT INTO "Movement" ("Name","Function") VALUES ($1, $2)', [name, program], (error, results) => {
         if (error) {
             throw error;
         }
-        response.status(201).json(results.rows);
+        response.status(201).send('User added with ID: ${results.rows[0]["id"]}');
     });
 };
 
